2b5581abf51ea472e2db068fffedeb1cff842936
Mike Nicolai <mike@topen.org>
2013-02-18 20:54:02 +0100

    added core profile example, which is a modified version of seanharmers version for Qt 4.8

---
+++ 01-core-profile/glwidget.cpp
@@ -0,0 +1,118 @@
+#include "glwidget.h"
+
+#include <QCoreApplication>
+#include <QKeyEvent>
+
+typedef void (*PglGenVertexArrays) (GLsizei n,  GLuint *arrays);
+typedef void (*PglBindVertexArray) (GLuint array);
+
+GLWidget::GLWidget( const QGLFormat& format, QWidget* parent )
+    : QGLWidget( format, parent ),
+      m_vertexBuffer( QOpenGLBuffer::VertexBuffer )
+{
+}
+
+void GLWidget::initializeGL()
+{
+    // get context opengl-version
+    qDebug() << "Widget OpenGl: " << format().majorVersion() << "." << format().minorVersion();
+    qDebug() << "Context valid: " << context()->isValid();
+    qDebug() << "Really used OpenGl: " << context()->format().majorVersion() << "." << context()->format().minorVersion();
+    qDebug() << "OpenGl information: VENDOR:       " << (const char*)glGetString(GL_VENDOR);
+    qDebug() << "                    RENDERDER:    " << (const char*)glGetString(GL_RENDERER);
+    qDebug() << "                    VERSION:      " << (const char*)glGetString(GL_VERSION);
+    qDebug() << "                    GLSL VERSION: " << (const char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    QGLFormat glFormat = QGLWidget::format();
+    if ( !glFormat.sampleBuffers() )
+        qWarning() << "Could not enable sample buffers";
+
+    // Set the clear color to black
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+
+    // Prepare a complete shader program...
+    if ( !prepareShaderProgram( ":/simple.vert", ":/simple.frag" ) )
+        return;
+
+    // we need a VAO in core!
+    GLuint VAO;
+    PglGenVertexArrays glGenVertexArrays = (PglGenVertexArrays) context()->getProcAddress("glGenVertexArrays");
+    PglBindVertexArray glBindVertexArray = (PglBindVertexArray) context()->getProcAddress("glBindVertexArray");
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // We need us some vertex data. Start simple with a triangle ;-)
+    float points[] = { -0.5f, -0.5f, 0.0f, 1.0f,
+                        0.5f, -0.5f, 0.0f, 1.0f,
+                        0.0f,  0.5f, 0.0f, 1.0f };
+    m_vertexBuffer.create();
+    m_vertexBuffer.setUsagePattern( QOpenGLBuffer::StaticDraw );
+    if ( !m_vertexBuffer.bind() )
+    {
+        qWarning() << "Could not bind vertex buffer to the context";
+        return;
+    }
+    m_vertexBuffer.allocate( points, 3 * 4 * sizeof( float ) );
+
+    // Bind the shader program so that we can associate variables from
+    // our application to the shaders
+    if ( !m_shader.bind() )
+    {
+        qWarning() << "Could not bind shader program to context";
+        return;
+    }
+
+    // Enable the "vertex" attribute to bind it to our currently bound
+    // vertex buffer.
+    m_shader.setAttributeBuffer( "vertex", GL_FLOAT, 0, 4 );
+    m_shader.enableAttributeArray( "vertex" );
+}
+
+void GLWidget::resizeGL( int w, int h )
+{
+    // Set the viewport to window dimensions
+    glViewport( 0, 0, w, qMax( h, 1 ) );
+}
+
+void GLWidget::paintGL()
+{
+    // Clear the buffer with the current clearing color
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+    // Draw stuff
+    glDrawArrays( GL_TRIANGLES, 0, 3 );
+}
+
+void GLWidget::keyPressEvent( QKeyEvent* e )
+{
+    switch ( e->key() )
+    {
+        case Qt::Key_Escape:
+            QCoreApplication::instance()->quit();
+            break;
+
+        default:
+            QGLWidget::keyPressEvent( e );
+    }
+}
+
+bool GLWidget::prepareShaderProgram( const QString& vertexShaderPath,
+                                     const QString& fragmentShaderPath )
+{
+    // First we load and compile the vertex shader...
+    bool result = m_shader.addShaderFromSourceFile( QOpenGLShader::Vertex, vertexShaderPath );
+    if ( !result )
+        qWarning() << m_shader.log();
+
+    // ...now the fragment shader...
+    result = m_shader.addShaderFromSourceFile( QOpenGLShader::Fragment, fragmentShaderPath );
+    if ( !result )
+        qWarning() << m_shader.log();
+
+    // ...and finally we link them to resolve any references.
+    result = m_shader.link();
+    if ( !result )
+        qWarning() << "Could not link shader program:" << m_shader.log();
+
+    return result;
+}
